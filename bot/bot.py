6c5ef4de05f854d9d0299b9434d25db961bc187d
Burning <artem.abuhovich@gmail.com>
2024-02-02 00:31:06 +0300

    protoss-gate-push-bo

--- bot/bot.py
+++ bot/bot.py
@@ -1,13 +1,13 @@
 from sc2.bot_ai import BotAI, Race
 from sc2.data import Result
+from sc2.ids.unit_typeid import UnitTypeId
+
 
 class CompetitiveBot(BotAI):
-    NAME: str = "CompetitiveBot"
-    """This bot's name"""
+    NAME: str = "BurningBot"
 
-    RACE: Race = Race.Terran
-    """This bot's Starcraft 2 race.
-    Options are:
+    RACE: Race = Race.Protoss
+    """
         Race.Terran
         Race.Zerg
         Race.Protoss
@@ -22,11 +22,13 @@ class CompetitiveBot(BotAI):
         print("Game started")
 
     async def on_step(self, iteration: int):
-        """
-        This code runs continually throughout the game
-        Populate this function with whatever your bot should do!
-        """
-        
+        await self.distribute_workers()
+        await self.build_workers(UnitTypeId.PROBE)
+        await self.build_supply_structure(UnitTypeId.PYLON)
+        await self.build_barracks()
+        await self.build_gas()
+        await self.build_cybercore()
+        await self.train_stalkers()
         pass
 
     async def on_end(self, result: Result):
@@ -35,3 +37,73 @@ class CompetitiveBot(BotAI):
         Do things here after the game ends
         """
         print("Game ended.")
+
+    async def get_workers_count(self):
+        worker_count: int = self.workers().count()
+        return worker_count
+
+    async def build_workers(self, workerType):
+        nexus = self.townhalls.ready.random
+        if (
+            self.can_afford(workerType)
+            and nexus.is_idle
+            and self.workers.amount < self.townhalls.amount * 22
+        ):
+            nexus.train(workerType)
+
+    async def build_supply_structure(self, supply_structure_type):
+        nexus = self.townhalls.ready.random
+        position = nexus.position.towards(self.enemy_start_locations[0], 10)
+
+        if (
+            self.supply_left < 3
+            and self.supply_cap <= 190
+            and self.already_pending(supply_structure_type) == 0
+            and self.can_afford(supply_structure_type)
+        ):
+            await self.build(supply_structure_type, near=position)
+
+    async def build_barracks(self):
+        if (
+            self.structures(UnitTypeId.PYLON).ready
+            and self.can_afford(UnitTypeId.GATEWAY)
+            and not self.structures(UnitTypeId.GATEWAY)
+        ):
+            pylon = self.structures(UnitTypeId.PYLON).ready.random
+            await self.build(UnitTypeId.GATEWAY, near=pylon)
+
+    async def build_gas(self):
+        if self.structures(UnitTypeId.GATEWAY):
+            for nexus in self.townhalls.ready:
+                vgs = self.vespene_geyser.closer_than(15, nexus)
+                for vg in vgs:
+                    if not self.can_afford(UnitTypeId.ASSIMILATOR):
+                        break
+                    worker = self.select_build_worker(vg.position)
+                    if worker is None:
+                        break
+                    if not self.gas_buildings or not self.gas_buildings.closer_than(1, vg):
+                        worker.build(UnitTypeId.ASSIMILATOR, vg)
+                        worker.stop(queue=True)
+
+    async def build_cybercore(self):
+        if self.structures(UnitTypeId.PYLON).ready:
+            pylon = self.structures(UnitTypeId.PYLON).ready.random
+            if self.structures(UnitTypeId.GATEWAY).ready:
+                if not self.structures(UnitTypeId.CYBERNETICSCORE):
+                    if (
+                        self.can_afford(UnitTypeId.CYBERNETICSCORE)
+                        and self.already_pending(UnitTypeId.CYBERNETICSCORE) == 0
+                    ):
+                        await self.build(UnitTypeId.CYBERNETICSCORE, near=pylon)
+
+    async def train_stalkers(self):
+        for gateway in self.structures(UnitTypeId.GATEWAY).ready:
+            if (
+                self.can_afford(UnitTypeId.STALKER)
+                and gateway.is_idle
+            ):
+                gateway.train(UnitTypeId.STALKER)
+
+    async def build_four_gates(self):
+        pass
